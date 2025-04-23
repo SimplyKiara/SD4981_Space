c050c91fdb4eac95ac43d84bb1a086390a653897
Clement458 <144639509+Clement458@users.noreply.github.com>
2025-04-24 01:40:43 +0800

    added rocket UI (not finished)

--- Assets/Scripts/CoverCraterUI.cs
+++ Assets/Scripts/CoverCraterUI.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 
 public class CoverCraterUI : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject StartingUI;
     public GameObject CraterUI;
+    public GameObject RocketUI;
+    private UICloser uICloser;
+    private bool suppliesPanelFound = false;
+    private bool rocketUIShown = false;
+
     void Start()
     {
         CraterUI.SetActive(false);
+        RocketUI.SetActive(false);
+        /*if (buttonHandler != null) {
+            buttonHandler.buttonPressedEvent.AddListener(OnButtonPressDetected);
+        }*/
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (StartingUI == null) {
+        // Monitor StartingUI destruction
+        if (StartingUI == null)
+        {
             Debug.Log("Starting UI has been destroyed!");
             CraterUI.SetActive(true);
         }
+
+        // Find all active GameObjects
+       GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        foreach (GameObject obj in allObjects)
+        {
+            // Check if SuppliesPanel appears in the hierarchy for the first time
+            if (!suppliesPanelFound && obj.name.Contains("SuppliesPanel"))
+            {
+                Debug.Log("Supplies Panel has been found for the first time!");
+                suppliesPanelFound = true; // Mark as found
+            }
+
+            // Check if SuppliesPanel has been destroyed (and RocketUI hasn't been shown yet)
+            if (suppliesPanelFound && !rocketUIShown && GameObject.Find("SuppliesPanel") == null)
+            {
+                Debug.Log("Supplies Panel has been destroyed, showing RocketUI!");
+                RocketUI.SetActive(true); // Activate RocketUI
+                rocketUIShown = true;     // Prevent RocketUI from popping up again
+            }
+        }
     }
+
+    /*void OnButtonPressDetected() {
+        Debug.Log("The UI is closed");
+        RocketUI.SetActive(true);
+    }*/
 }
