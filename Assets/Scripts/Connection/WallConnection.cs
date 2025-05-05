d5350298fe7b75826cdd72da16fb00bf51fa6cc8
RY <lynruby374@gmail.com>
2025-05-05 18:06:33 +0800

    wall connection

---
+++ Assets/Scripts/Connection/WallConnection.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIOClient;
+
+public class WallConnection : MonoBehaviour
+{
+    private SocketIOUnity socket;
+    private string baseUrl = "http://localhost:3000"; // Update with your server URL
+
+    void Start()
+    {
+        InitializeSocket();
+    }
+
+    private void InitializeSocket()
+    {
+        var uri = new System.Uri(baseUrl);
+        socket = new SocketIOUnity(uri, new SocketIOOptions
+        {
+            Query = new System.Collections.Generic.Dictionary<string, string>
+            {
+                { "token", "UNITY" }
+            },
+            Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
+        });
+
+        socket.OnConnected += (sender, e) =>
+        {
+            Debug.Log("Connection open!");
+        };
+
+        socket.OnError += (sender, e) =>
+        {
+            Debug.LogError("Error! " + e);
+        };
+
+        socket.OnDisconnected += (sender, e) =>
+        {
+            Debug.Log("Connection closed!");
+        };
+
+        socket.On("gameStarted", response =>
+        {
+            Debug.Log("Game started message received!");
+            HandleGameStarted();
+        });
+
+        socket.ConnectAsync();
+    }
+
+    private void HandleGameStarted()
+    {
+        // Implement your logic for when the game starts
+        Debug.Log("Handling game start logic...");
+    }
+}
